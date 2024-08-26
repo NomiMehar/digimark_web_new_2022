b6a3afc93fbc5d0db3a200babe74d53072bbfc64
Muhammad Nouman <59343983+NomiMehar@users.noreply.github.com>
2024-08-26 18:22:39 +0500

    sitemap location

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,5 +2,6 @@ module.exports = {
     siteUrl: 'https://digimark-web-new-2022.vercel.app/', // Replace with your site's URL
     generateRobotsTxt: true, // (optional) Generate a robots.txt file
     sitemapSize: 5000, // Maximum entries per sitemap file (default is 5000)
+    outDir: 'public', // Ensure the sitemap is generated in the public directory
   };
   
