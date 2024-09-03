66e95c6ba7ac605783c6475e4dea9fa460050d19
Muhammad Nouman <59343983+NomiMehar@users.noreply.github.com>
2024-09-03 20:06:08 +0500

    Update Banner.js

--- pages/landingpage/Banner/Banner.js
+++ pages/landingpage/Banner/Banner.js
@@ -16,7 +16,19 @@ export default function Banner() {
       </video>
       <div className="flex direction-column container">
         <section className={style.banner_content}>
-
+          <h1 className="font-white">
+              Bridging the Gap Between
+            <span className="font-extra-bold font-green">
+                 {/* <Typewriter
+                     options={{
+                         strings: ['Business & Technology', 'Business & Digital Solutions'],
+                         autoStart: true,
+                         loop: true,
+                     }}
+                 /> */}
+            </span>
+              with Our Innovative Solutions!
+          </h1>
           <p className="font-white">
           We create <span>Digital Solutions</span> that meet business objectives and ensure impactful transformation towards a better future.          </p>
           {/* <button
